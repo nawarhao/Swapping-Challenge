d09a694ca19172b0183a035c6d4f96a5ef6898e5
razonno <nrazonno@gmail.com>
2025-05-10 12:00:15 -0400

    Add files via upload

---
+++ swappingchallenges.py
@@ -0,0 +1,18 @@
+# Initial values (mistakenly interchanged)
+a = 5
+b = 10
+c = 15
+
+# Display values before swapping
+print("Before swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+# Swap 
+start = a
+a = b
+b = c
+c = start
+
+# Display values after swapping
+print("After swapping:")
+print("a =", a, "b =", b, "c =", c)
